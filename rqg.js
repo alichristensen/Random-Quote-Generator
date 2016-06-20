82d3c0fc9fcc5f02d1fc612bf83c483a916fcd3c
alichristensen <ali.christensen1@gmail.com>
2016-06-20 12:09:17 -0500

    quotes added

--- rqg.js
+++ rqg.js
@@ -6,7 +6,9 @@ var quotes = [
 	"It's like a new pair of underwear, at first they're really restricive but then after a while they become a part of you - Garth Algar", 
 	"Garth, marriage is punishment for shoplifting in some countries- Wayne Campbell", 
 	"Hey Mr. Donut man, who's trying to kill ya? I don't know but they better not! - Garth Algar", 
-	"I'm having a good time... NOT! - Garth Algar"
+	"I'm having a good time... NOT! - Garth Algar", 
+	"I know I don't have his looks. I know I don't have his money. I know I don't have his connections, his knowledge of fine wines. I know sometimes when I eat I get this clicking sound in my jaw... - Wayne Campbell", 
+	"We're not worthy! We're not worthy!- Wayne Campbell, Garth Algar"
 	]; 
 
 
@@ -27,7 +29,7 @@ function generator() {
 		h3.textContent = quotes[num]; 
 	}
 	else {
-		h3.textContent = quotes[(num +1)%5]; 
+		h3.textContent = quotes[(num +1)]; 
 	}
 	previous = num;
 }
